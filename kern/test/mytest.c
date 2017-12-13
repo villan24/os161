a0c3f7f7f678324e385ffaa4c5f084e7d536e4c4
villan24 <32624131+villan24@users.noreply.github.com>
2017-12-12 22:13:03 -0800

    Update mytest.c

--- kern/test/mytest.c
+++ kern/test/mytest.c
@@ -1,1 +1,16 @@
+#include <types.h>
+#include <lib.h>
+#include <test.h>
 
+
+int
+mytest(int nargs, char **args)
+{
+  //left in because i dont know what it does yet..
+	(void)nargs;
+	(void)args;
+
+	kprintf("Beginning lab 4 test...\n");
+	kprintf("Lab 4 test complete\n");
+	return 0;
+}
