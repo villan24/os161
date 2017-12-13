e7ca56a6e38c24ef39c6e7e322c09071d38dbc64
villan24 <32624131+villan24@users.noreply.github.com>
2017-12-12 22:58:18 -0800

    Update mytest.c

--- kern/test/mytest.c
+++ kern/test/mytest.c
@@ -7,9 +7,12 @@ int
 mytest(int nargs, char **args)
 {
   //left in because i dont know what it does yet..
-	(void)nargs;
-	(void)args;
-	kprintf(args[1]);
+	//(void)nargs;
+	while(nargs--)
+                printf("%s\n", *args++);
+        exit(EXIT_SUCCESS);
+	//(void)args;
+	
 	kprintf("Beginning lab 4 test...\n");
 	kprintf("Lab 4 test complete\n");
 	return 0;
